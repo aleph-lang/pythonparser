7f7935d6dc71e2c8a25bf7804f4f876fa94b0bf0
Roques Steve <steve.roques@gmail.com>
2023-05-01 07:56:49 +0200

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  # choose the ocaml version you want to use
+  my-python = nixpkgs.python310;
+  python-with-packages = my-python.withPackages ( p: with p; [
+    aiohttp
+  ]);
+in
+  nixpkgs.mkShell {
+    # dependencies
+    buildInputs = with nixpkgs; [ 
+        #nixpkgs.latest.rustChannels.nightly.rust
+	(nixpkgs.rustChannelOf { date = "2023-04-26"; channel = "nightly"; }).rust
+        cargo
+        rustc
+        python-with-packages
+        pkg-config
+        openssl
+    ];
+  }
